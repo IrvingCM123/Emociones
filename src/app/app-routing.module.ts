44d76ab29781b178a3ee270cbeaa79df427d8115
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-06-16 21:28:49 -0600

    cambio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Tema1Component } from './tema1/tema1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'tema1', component: Tema1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
