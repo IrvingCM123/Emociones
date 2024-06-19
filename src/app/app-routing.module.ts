f632c577f96485c09d1da354a1ead57702bb9043
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-06-19 00:11:32 -0600

    cambios ma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,18 @@ import { Tema1Component } from './tema1/tema1.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'Conciencia emocional', component: Tema1Component}
+  { path: 'tema1', component: Tema1Component},
+  { path: 'tema2', component: Tema1Component},
+  { path: 'Conciencia emocional', component: Tema1Component},
+  { path: 'Regulación emocional', component: Tema1Component},
+  { path: 'Autonomía emocional', component: Tema1Component},
+  { path: 'Competencias social', component: Tema1Component},
+  { path: 'Habilidades para la vida y el bienestar', component: Tema1Component},
+  { path: 'Conciencia emocional.', component: Tema1Component},
+  { path: 'Regulación emocional.', component: Tema1Component},
+  { path: 'Autonomía emocional.', component: Tema1Component},
+  { path: 'Competencias social.', component: Tema1Component},
+  { path: 'Habilidades para la vida y el bienestar.', component: Tema1Component},
 ];
 
 @NgModule({
--- src/app/tema1/tema1.component.ts
+++ src/app/tema1/tema1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../datos/datos.service';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tema1',
@@ -10,17 +11,36 @@ import { ActivatedRoute } from '@angular/router';
 export class Tema1Component implements OnInit {
 
   data: any;
+  enlace: string = '';
 
   constructor(
     private dataService: DataService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
+
+  private validRoutes: Set<string> = new Set([
+    'home',
+    'tema1',
+    'tema2',
+    'Conciencia emocional',
+    'Regulación emocional',
+    'Autonomía emocional',
+    'Competencias social',
+    'Habilidades para la vida y el bienestar',
+    'Conciencia emocional.',
+    'Regulación emocional.',
+    'Autonomía emocional.',
+    'Competencias social.',
+    'Habilidades para la vida y el bienestar.'
+  ]);
 
   ngOnInit(): void {
     this.route.url.subscribe(url => {
-      const routePath = url[0] ? url[0].path : 'TemaA';
+      const routePath = url[0] && this.validRoutes.has(url[0].path) ? url[0].path : 'tema1';
+      console.log(routePath);
       this.data = this.dataService.getData(routePath);
     });
+    console.log(this.data)
   }
-
 }
